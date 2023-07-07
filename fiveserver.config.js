61ced09bfcaf32e0d753664618cf7afffc03a21d
TheProtectorOfEarth <alyfsakha1@gmail.com>
2023-07-07 16:56:17 +0700

    logic for display books

--- fiveserver.config.js
+++ fiveserver.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    php: "C:\\xampp\\php\\php.exe"   // Windows
-  }
+  php: "d:/File_Ku/Apps/xampp/php/php.exe"  
+}
--- pages/books.php
+++ pages/books.php
@@ -1,4 +1,29 @@
 <?php
+    $conn = mysqli_connect('localhost', 'root', '', 'library');
+    $cari='SELECT * FROM book';
+    $hasil_cari=mysqli_query($conn, $cari);
+    // while($data=mysqli_fetch_row($hasil_cari)){
+    //     echo '<div class="book1 m-10 w-72 bg-white border-2 drop-shadow-2xl border-gray-700 flex flex-col justify-between items-center py-8 rounded-xl"> <img src="/dist/image/mata-yang-enak-dipandang.jpg" alt="" class="w-56"> <div class="text"> <p class="text-xl font-bold mt-4">', $data[1] , '</p> <p class="text-xl">Ahmad Tohari</p> <button class="button text-lg font-semibold mx-auto w-full text-white bg-gray-900 hover:bg-gray-600 py-2 rounded-md mt-3 transition-all"><a href="book-view.php">Selengkapnya</a></button> </div> </div>';
+    // }
+    
+    // Check if there are any rows returned
+    if (mysqli_num_rows($hasil_cari) > 0) {
+        // Fetch all rows into an array
+        $items = mysqli_fetch_all($hasil_cari, MYSQLI_ASSOC);
+    } else {
+        $items = array(); // Empty array if no items found
+    }
+
+
+    // Convert the items array to JSON format
+    $items_json = json_encode($items);
+
+    
+
+    // Pass the JSON data to JavaScript
+    echo "<script>";
+    echo "var items = " . $items_json . ";";
+    echo "</script>";
 ?>
 
 <!DOCTYPE html>
@@ -152,14 +177,38 @@
 
     </footer> -->
 
-    <script type="text/javascript">
-        var list__contianer = document.getElementById("list__container");
+    <!-- <script type="text/javascript">
+        var list__container = document.getElementById("list__container");
         var phd = '';
+        
         for (let x = 0; x < 20; x++) {
-            phd = phd + '<div class="book1 m-10 w-72 bg-white border-2 drop-shadow-2xl border-gray-700 flex flex-col justify-between items-center py-8 rounded-xl"> <img src="/dist/image/mata-yang-enak-dipandang.jpg" alt="" class="w-56"> <div class="text"> <p class="text-xl font-bold mt-4">Mata Yang Enak Dipandang</p> <p class="text-xl">Ahmad Tohari</p> <button class="button text-lg font-semibold mx-auto w-full text-white bg-gray-900 hover:bg-gray-600 py-2 rounded-md mt-3 transition-all"><a href="book-view.php">Selengkapnya</a></button> </div> </div>';
-            list__contianer.innerHTML = phd;
+            phd = phd + '<div class="book1 m-10 w-72 bg-white border-2 drop-shadow-2xl border-gray-700 flex flex-col justify-between items-center py-8 rounded-xl"> <img src="/dist/image/mata-yang-enak-dipandang.jpg" alt="" class="w-56"> <div class="text"> <p class="text-xl font-bold mt-4">', $items[1].book_title , '</p> <p class="text-xl">Ahmad Tohari</p> <button class="button text-lg font-semibold mx-auto w-full text-white bg-gray-900 hover:bg-gray-600 py-2 rounded-md mt-3 transition-all"><a href="book-view.php">Selengkapnya</a></button> </div> </div>';
+            
         };
-    </script>
+    </script> -->
+
+    <script type="text/javascript">
+    var list__container = document.getElementById("list__container");
+    var phd = '';
+    
+    var count = 0;
+    for (var x = 0; x < items.length; x++) {
+        phd += '<div class="book1 m-10 w-72 bg-white border-2 drop-shadow-2xl border-gray-700 flex flex-col justify-between items-center py-8 rounded-xl">';
+        phd += "<img src='/dist/image/'"+items[count].imageurl+ 'alt="" class="w-56">';
+        phd += '<div class="text p-6 w-full">';
+        phd += '<p class="text-xl font-bold mt-4">' + items[count].book_title + '</p>';
+        phd += '<p class="text-xl">'+ items[count].book_author +'</p>';
+        phd += '<button class="button text-lg font-semibold mx-auto w-full text-white bg-gray-900 hover:bg-gray-600 py-2 rounded-md mt-3 transition-all"><a href="book-view.php">Selengkapnya</a></button>';
+        phd += '</div>';
+        phd += '</div>';
+        count ++;
+    }
+
+    
+
+    list__container.innerHTML = phd;
+</script>
+
 
 
 </body>
